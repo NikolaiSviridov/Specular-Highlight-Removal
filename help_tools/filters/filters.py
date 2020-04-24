c2004d6d709bc45663ad26be3df2b32f2033d1c7
NikolaiSv <nikolaisvg@gmail.com>
2020-04-24 15:43:24 +0300

    just save prev results

---
+++ help_tools/filters/filters.py
@@ -0,0 +1,8 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+img = cv.imread('die.png')
+dst = cv.fastNlMeansDenoisingColored(img,None,10,10,7,21)
+plt.subplot(121),plt.imshow(img)
+plt.subplot(122),plt.imshow(dst)
+plt.show()
