5a27b75931193f8fee70cc62e698607b9e09b793
NikolaiSv <nikolaisvg@gmail.com>
2020-05-17 10:08:21 +0300

    save gitignore

--- help_tools/filters/filters.py
+++ help_tools/filters/filters.py
@@ -1,8 +1,9 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
-img = cv.imread('die.png')
-dst = cv.fastNlMeansDenoisingColored(img,None,10,10,7,21)
-plt.subplot(121),plt.imshow(img)
-plt.subplot(122),plt.imshow(dst)
+
+img = cv.imread('p2.jpg')
+dst = cv.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 21)
+plt.subplot(121), plt.imshow(img)
+plt.subplot(122), plt.imshow(dst)
 plt.show()
